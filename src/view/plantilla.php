d474974250720f64110990d11aebc72fb70ade2a
Maytemoises2020 <astrid.farfan.taipe@gmail.com>
2025-06-17 12:50:41 -0500

    17imprimir

--- src/view/plantilla.php
+++ src/view/plantilla.php
@@ -54,8 +54,13 @@ if ($reset =="reset_passsword") {
 if ($vista == "login" || $vista == "404" || $vista == "reset-password") {
     require_once "./src/view/" . $vista . ".php";
 } else {
+    if ($vista !='./src/view/imprimir-movimiento.php') {
+        include "./src/view/include/header.php";
+    }
 
-    include "./src/view/include/header.php";
-    include $vista;
-    include "./src/view/include/footer.php";
+    include $vista; 
+    if ($vista !='./src/view/imprimir-movimiento.php') {
+         include "./src/view/include/footer.php";
+    }     
+   
 }
