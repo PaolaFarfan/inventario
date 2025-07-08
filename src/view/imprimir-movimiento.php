ea32edb1ca6bec06f2999362fd3738d651e5989b
Maytemoises2020 <astrid.farfan.taipe@gmail.com>
2025-07-08 17:00:08 -0500

    8 reporte

--- src/view/imprimir-movimiento.php
+++ src/view/imprimir-movimiento.php
@@ -1,38 +1,38 @@
 <?php
+ob_start();
 $ruta = explode("/", $_GET['views']);
-if (!isset($ruta[1]) || $ruta[1]=="") {
-    header("location: " . BASE_URL."movimientos" );
+if (!isset($ruta[1]) || $ruta[1] == "") {
+  header("location: " . BASE_URL . "movimientos");
 }
 $curl = curl_init(); //inicia la sesión cURL
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => BASE_URL_SERVER."src/control/Movimiento.php?tipo=buscar_movimiento_id&sesion=".$_SESSION['sesion_id']."&token=".$_SESSION['sesion_token']."&data=".$ruta[1], //url a la que se conecta
-        CURLOPT_RETURNTRANSFER => true, //devuelve el resultado como una cadena del tipo curl_exec
-        CURLOPT_FOLLOWLOCATION => true, //sigue el encabezado que le envíe el servidor
-        CURLOPT_ENCODING => "", // permite decodificar la respuesta y puede ser"identity", "deflate", y "gzip", si está vacío recibe todos los disponibles.
-        CURLOPT_MAXREDIRS => 10, // Si usamos CURLOPT_FOLLOWLOCATION le dice el máximo de encabezados a seguir
-        CURLOPT_TIMEOUT => 30, // Tiempo máximo para ejecutar
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, // usa la versión declarada
-        CURLOPT_CUSTOMREQUEST => "GET", // el tipo de petición, puede ser PUT, POST, GET o Delete dependiendo del servicio
-        CURLOPT_HTTPHEADER => array(
-            "x-rapidapi-host: ".BASE_URL_SERVER,
-            "x-rapidapi-key: XXXX"
-        ), //configura las cabeceras enviadas al servicio
-    )); //curl_setopt_array configura las opciones para una transferencia cURL
+curl_setopt_array($curl, array(
+  CURLOPT_URL => BASE_URL_SERVER . "src/control/Movimiento.php?tipo=buscar_movimiento_id&sesion=" . $_SESSION['sesion_id'] . "&token=" . $_SESSION['sesion_token'] . "&data=" . $ruta[1], //url a la que se conecta
+  CURLOPT_RETURNTRANSFER => true, //devuelve el resultado como una cadena del tipo curl_exec
+  CURLOPT_FOLLOWLOCATION => true, //sigue el encabezado que le envíe el servidor
+  CURLOPT_ENCODING => "", // permite decodificar la respuesta y puede ser"identity", "deflate", y "gzip", si está vacío recibe todos los disponibles.
+  CURLOPT_MAXREDIRS => 10, // Si usamos CURLOPT_FOLLOWLOCATION le dice el máximo de encabezados a seguir
+  CURLOPT_TIMEOUT => 30, // Tiempo máximo para ejecutar
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, // usa la versión declarada
+  CURLOPT_CUSTOMREQUEST => "GET", // el tipo de petición, puede ser PUT, POST, GET o Delete dependiendo del servicio
+  CURLOPT_HTTPHEADER => array(
+    "x-rapidapi-host: " . BASE_URL_SERVER,
+    "x-rapidapi-key: XXXX"
+  ), //configura las cabeceras enviadas al servicio
+)); //curl_setopt_array configura las opciones para una transferencia cURL
 
-    $response = curl_exec($curl); // respuesta generada
-    $err = curl_error($curl); // muestra errores en caso de existir
+$response = curl_exec($curl); // respuesta generada
+$err = curl_error($curl); // muestra errores en caso de existir
 
-    curl_close($curl); // termina la sesión 
+curl_close($curl); // termina la sesión 
 
-    if ($err) {
-        echo "cURL Error #:" . $err; // mostramos el error
-    } else {
-        $respuesta =json_decode($response);
-       // print_r($respuesta);// en caso de funcionar correctamente
-
-    ?>
-<!--
-<!DOCTYPE html>
+if ($err) {
+  echo "cURL Error #:" . $err; // mostramos el error
+} else {
+  $respuesta = json_decode($response);
+  //print_r($respuesta);// en caso de funcionar correctamente
+  $contenido_pdf = '';
+  $contenido_pdf .= '
+       <!DOCTYPE html>
 <html lang="es">
 <head>
   <meta charset="UTF-8">
@@ -87,45 +87,48 @@ $curl = curl_init(); //inicia la sesión cURL
   </style>
 </head>
 <body>
-
   <h2>PAPELETA DE ROTACION DE BIENES</h2>
-
   <div class="info">
     <p><b>ENTIDAD</b>: <span class="underline">DIRECCION REGIONAL DE EDUCACION - AYACUCHO</span></p>
     <p><b>AREA</b>: <span class="underline">OFICINA DE ADMINISTRACIÓN</span></p>
-    <p><b>ORIGEN</b>: <span class="underline"><?php echo $respuesta->amb_origen->codigo. "_" .$respuesta->amb_origen->detalle?></span></p>
-    <p><b>DESTINO</b>: <span class="underline"><?php echo $respuesta->amb_destino->codigo."_" .$respuesta->amb_destino->detalle?></span></p>
-    <p><b>MOTIVO (*)</b>: <span class="underline"><?php echo $respuesta->movimiento->descripcion?></span></p>
+    <p><b>ORIGEN</b>: <span class="underline">' . $respuesta->amb_origen->codigo . ' -' . $respuesta->amb_origen->detalle . '</span></p>
+    <p><b>DESTINO</b>: <span class="underline">' . $respuesta->amb_destino->codigo . '- ' . $respuesta->amb_destino->detalle . '</span></p>
+    <p><b>MOTIVO (*)</b>: <span class="underline">' . $respuesta->movimiento->descripcion . '</span></p>
   </div>
-
   <table>
-    <tr>
-      <th>ITEM</th>
-      <th>CODIGO PATRIMONIAL</th>
-      <th>NOMBRE DEL BIEN</th>
-      <th>MARCA</th>
-      <th>COLOR</th>
-      <th>MODELO</th>
-      <th>ESTADO</th>
-    </tr>
-    <tr>
+    <thead>
+      <tr>
+        <th>ITEM</th>
+        <th>CODIGO PATRIMONIAL</th>
+        <th>NOMBRE DEL BIEN</th>
+        <th>MARCA</th>
+        <th>COLOR</th>
+        <th>MODELO</th>
+        <th>ESTADO</th>
+      </tr>
+    </thead>
     <tbody>
+       ';
+
+?>
+
+
 
   <?php
   $contador = 1;
-  foreach ($respuesta->bienes as $bien) {
-      echo "<tr>";
-      echo "<td>" . $contador++ . "</td>";
-      echo "<td>" . $bien->cod_patrimonial . "</td>";
-      echo "<td>" . $bien->denominacion . "</td>";
-      echo "<td>" . $bien->marca . "</td>";
-      echo "<td>" . $bien->color . "</td>";
-      echo "<td>" . $bien->modelo . "</td>";
-      echo "<td>" . $bien->estado_conservacion . "</td>";
-      echo "</tr>";
+  foreach ($respuesta->detalle as $bien) {
+    $contenido_pdf .= "<tr>";
+    $contenido_pdf .= "<td>" . $contador++ . "</td>";
+    $contenido_pdf .=  "<td>" . $bien->cod_patrimonial . "</td>";
+    $contenido_pdf .= "<td>" . $bien->denominacion . "</td>";
+    $contenido_pdf .= "<td>" . $bien->marca . "</td>";
+    $contenido_pdf .= "<td>" . $bien->color . "</td>";
+    $contenido_pdf .= "<td>" . $bien->modelo . "</td>";
+    $contenido_pdf .= "<td>" . $bien->estado_conservacion . "</td>";
+    $contenido_pdf .= "</tr>";
   }
-  ?>
-  </tbody>
+  $contenido_pdf .= '
+   </tbody>
 </table>
 
   <div class="lugar-fecha">
@@ -145,27 +148,35 @@ $curl = curl_init(); //inicia la sesión cURL
 
 </body>
 </html>
--->
-    <?php
-    require_once('./vendor/tecnickcom/tcpdf/tcpdf.php');
+  ';
+  require_once('./vendor/tecnickcom/tcpdf/tcpdf.php');
 
-    $pdf = new TCPDF();
+  $pdf = new TCPDF();
 
-    // set document information
-$pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Paola Farfan');
-$pdf->SetTitle('Reporte de movimiento');// concatenar con la fecha
+  // set document information
+  $pdf->SetCreator(PDF_CREATOR);
+  $pdf->SetAuthor('Paola Farfan');
+  $pdf->SetTitle('Reporte de movimiento'); // concatenar con la fecha
 
-// set margins
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+  // set margins
+  $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
 
-// salto de pagina automatico
-$pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-
-
-// set font
-$pdf->SetFont('dejavusans', '', 10);
+  // salto de pagina automatico
+  $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
 
 
+  // set font
+  $pdf->SetFont('helvetica', 'B', 12);
 
-    }
+  // add a page
+  $pdf->AddPage();
+
+
+  // output the HTML content
+  $pdf->writeHTML($contenido_pdf, true, false, true, false, '');
+  if (ob_get_length()) {
+    ob_clean();
+  }
+  //Close and output PDF document
+  $pdf->Output('example_006.pdf', 'I');
+}
