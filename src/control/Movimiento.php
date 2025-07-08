52ac53d560e08ada9bfe00c6ef0a1b386bc98c5d
Maytemoises2020 <astrid.farfan.taipe@gmail.com>
2025-07-08 15:30:24 -0500

    18 reporte

--- src/control/Movimiento.php
+++ src/control/Movimiento.php
@@ -205,8 +205,8 @@ if ($tipo == "datos_registro") {
     }
     echo json_encode($arr_Respuesta);
 }
-if ($tipo== "buscar_movimiento_id") {
-    $arr_Respuesta = array('status' => false, 'msg' =>'Error_Sesion');
+if ($tipo == "buscar_movimiento_id") {
+    $arr_Respuesta = array('status' => false, 'msg' => 'Error_Sesion');
     if ($objSesion->verificar_sesion_si_activa($id_sesion, $token)) {
         $id_movimiento = $_REQUEST['data'];
         $arrMovimiento = $objMovimiento->buscarMovimientoById($id_movimiento);
@@ -215,17 +215,18 @@ if ($tipo== "buscar_movimiento_id") {
         $arrUsuario = $objUsuario->buscarUsuarioById($arrMovimiento->id_usuario_registro);
         $arrIes= $objInstitucion->buscarInstitucionById($arrMovimiento->id_ies);
         $arrDetalle = $objMovimiento->buscarDetalle_MovimientoByMovimiento($id_movimiento);
-        $array_bien = array();
+        $array_bienes = array();
         foreach ($arrDetalle as $bien) {
-            $id_bien =$bien->id_bien;
-            $res_bien =$objBien->buscarBienById($id_bien);
+            $id_bien = $bien->id_bien;
+            $res_bien = $objBien->buscarBienById($id_bien);
+            array_push($array_bienes, $res_bien);
         }
         $arr_Respuesta['movimiento'] = $arrMovimiento;
         $arr_Respuesta['amb_origen'] = $arrAmbOrigen;
         $arr_Respuesta['amb_destino'] = $arrAmbDestino;
         $arr_Respuesta['datos_usuario'] = $arrUsuario;
         $arr_Respuesta['datos_ies'] = $arrIes;
-        $arr_Respuesta['detalle'] = $arrDetalle;
+        $arr_Respuesta['detalle'] = $array_bienes;
         $arr_Respuesta['status'] = true;
         $arr_Respuesta['msg'] = 'correcto';
     }
